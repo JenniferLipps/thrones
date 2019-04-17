d7300305399b807afcf0d603df27e2a7f716ff26
Jennifer Lipps <jjlipps@gmail.com>
2019-04-16 19:43:33 -0500

    added js

---
+++ src/javascripts/main.js
@@ -0,0 +1,10 @@
+import characters from './components/characters/characters';
+import '../styles/main.scss';
+
+console.error('nope');
+
+const init = () => {
+  characters.getData();
+};
+
+init();
